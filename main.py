3628a2937576f89b67be2462e8da8ee2889ec443
Mpampis1312 <xarisban@gmail.com>
2025-08-05 17:53:10 +0300

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from Teen_Phone_Addiction import logger
+from Teen_Phone_Addiction.pipeline.stage_01_ingestion_data import (
+    DataIngestionTrainingPipeline,
+)
 
-logger.info("Logging has been set up successfully.")
+Stage_name = "Data Ingestion stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>>>> stage {Stage_name} started <<<<<<<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>>>> stage {Stage_name} completed <<<<<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
